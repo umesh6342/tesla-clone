2660fcf980474c28fb7d7872ef67f6f88b0e13a1
Umesh patel <100286708+umesh6342@users.noreply.github.com>
2022-03-10 17:40:54 +0530

    Updates

---
+++ precache-manifest.f093e1d7cabf9b38a383c734ffe18461.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a28c68707c6000894ac1",
+    "url": "/tesla-clone/static/css/main.c1808496.chunk.css"
+  },
+  {
+    "revision": "a28c68707c6000894ac1",
+    "url": "/tesla-clone/static/js/main.a28c6870.chunk.js"
+  },
+  {
+    "revision": "c6b5a3e0f92ee25b3986",
+    "url": "/tesla-clone/static/js/1.c6b5a3e0.chunk.js"
+  },
+  {
+    "revision": "fc0326e92420dc869cc7",
+    "url": "/tesla-clone/static/js/2.fc0326e9.chunk.js"
+  },
+  {
+    "revision": "529d7d7c9f9f14e0c71c",
+    "url": "/tesla-clone/static/js/runtime~main.529d7d7c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/tesla-clone/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "08ff95542ea5f4d5022d9b0de71c2154",
+    "url": "/tesla-clone/index.html"
+  }
+];
